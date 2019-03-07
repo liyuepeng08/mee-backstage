72ba8da478384f1601ac1be9c4578e7fb35f444b
zhucongcong <15110253002@163.com>
2019-03-07 18:13:01 +0800

    新增公共模块

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Element from 'element-ui'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '../static/css/reset.css';
 
 Vue.config.productionTip = false
 
