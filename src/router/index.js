5640c4fc7f5fd1b12d0224f9be0e862e0ddc5ca0
yuanna0323 <15010611339@163.com>
2019-03-08 17:13:14 +0800

    修改路由和app.vue给父元素#app定义高度100%

--- src/router/index.js
+++ src/router/index.js
@@ -4,64 +4,62 @@ import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import Header from 'base/Header/Header'
 import DatePicker from 'base/datePicker/datePicker'; //时间选择器
-import Admin from '@/components/admin/admin'    //后台首页
-import Agent from '@/components/agent/agent'    //代理商管理
-import School from '@/components/school/school'   //学校管理
-import Account from '@/components/account/account'    //账户管理
-import Examine from '@/components/examine/examine'      //审核管理
-import Security from '@/components/security/security'     //安全管理
+import Admin from '@/components/admin/admin' //后台首页
+import Agent from '@/components/agent/agent' //代理商管理
+import School from '@/components/school/school' //学校管理
+import Account from '@/components/account/account' //账户管理
+import Examine from '@/components/examine/examine' //审核管理
+import Security from '@/components/security/security' //安全管理
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'DatePicker',
       component: DatePicker
     },
     {
-      path: '/admin',   //后台主页面
+      path: '/admin', //后台主页面
       name: 'admin',
       component: Admin,
-      children: [
-        {     
-          path: '/',    //代理商管理
+      children: [{
+          path: '/', //代理商管理
           name: 'agent',
           component: Agent
         },
         {
-          path: 'school',   //学校管理
+          path: 'school', //学校管理
           name: 'school',
           component: School
         },
         {
-          path: 'account',   //账号管理
+          path: 'account', //账号管理
           name: 'account',
           component: Account
         },
         {
-          path: 'examine',  //审核管理
+          path: 'examine', //审核管理
           name: 'examine',
           component: Examine
         },
         {
-          path: 'security',   //安全设置
+          path: 'security', //安全设置
           name: 'security',
           component: Security
-        }, {
-		    path: '/login',
-		    name: 'Login',
-		    component: Login
-		  }, {
-		    path: '/register',
-		    name: 'Register',
-		    component: Register
-		  }, {
-		    path: '/header',
-		    name: 'Header',
-		    component: Header
-		  }
+        }
       ]
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }, {
+      path: '/header',
+      name: 'Header',
+      component: Header
     }
   ]
 })
