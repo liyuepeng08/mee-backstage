0da56ba774cfd2af0b9894d5b55732963fe5114c
yuanna0323 <15010611339@163.com>
2019-03-08 16:58:58 +0800

    提交头部和登录注册页面

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,7 +4,12 @@
       <img class="logo" src="../assets/images/loginLogo.png" alt>
       <div class="account">
         <input type="text" class="userName" placeholder="请输入手机号/账号">
+        <div class="phone">
+          <input class="verification" type="text" placeholder="请输入验证码">
+          <input class="sendVerification" type="button" value="发送验证码">
+        </div>
         <input type="password" placeholder="请输入密码">
+        <input type="password" placeholder="确认密码">
       </div>
       <select name="school" id="school" class="school tipsColor">
         <option class="tips" style="display: none;" disabled selected>请选择默认选项或机构</option>
@@ -12,10 +17,11 @@
         <option value="2">2</option>
         <option value="3">3</option>
       </select>
-      <input type="button" value="登 录" class="button">
+      <div class="addMechanism">+ 添加所属机构</div>
+      <input type="button" value="注 册" class="button">
       <div class="aboutLink">
-        <a href class="registerLink">注册账号</a>|
-        <a href>忘记密码？</a>
+        已有账号：
+        <a href>去登录-></a>
       </div>
     </div>
   </div>
@@ -23,7 +29,7 @@
 
 <script>
 export default {
-  name: "Login",
+  name: "Register",
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
@@ -41,7 +47,7 @@ export default {
   background-color: #196197;
   .loginBody {
     width: 300px;
-    height: 460px;
+    height: 600px;
     background-color: #f0f0f0;
     box-shadow: 0px 6px 10px 0px rgba(0, 0, 0, 0.2);
     border-radius: 10px;
@@ -49,33 +55,46 @@ export default {
     top: 50%;
     left: 50%;
     margin-left: -320px;
-    margin-top: -230px;
+    margin-top: -300px;
     padding: 0 170px;
     .logo {
       padding: 40px 90px;
     }
     .account {
-      width: 298px;
-      height: 100px;
-      background-color: #e8e8e8;
-      border-radius: 4px;
-      border: solid 1px #b8b8b8;
-      margin-bottom: 20px;
+      // height: 100px;
       input {
+        border-radius: 4px;
+        border: solid 1px #b8b8b8;
+        margin-bottom: 10px;
         display: block;
-        border: none;
         width: 277px;
-        margin: 0 10px;
-        height: 47%;
+        padding: 0 10px;
+        height: 47px;
         background-color: #e8e8e8;
         text-indent: 10px;
         outline: none;
       }
-      .userName {
-        background-color: #e8e8e8;
-        border-bottom: solid 1px #b8b8b8;
+      .phone {
+        display: flex;
+        align-items: center;
+        .verification {
+          width: 180px;
+        }
+        .sendVerification {
+          width: 110px;
+          height: 50px;
+          background-color: #2e8ed6;
+          border-radius: 4px;
+          text-indent: 0px;
+          font-size: 12px;
+          line-height: 48px;
+          color: #ffffff;
+          margin-left: 10px;
+          cursor: pointer;
+        }
       }
     }
+
     .school {
       width: 298px;
       height: 50px;
@@ -84,13 +103,26 @@ export default {
       border: solid 1px #b8b8b8;
       padding: 0 10px;
       text-indent: 6px;
-      margin-bottom: 40px;
+      margin-bottom: 10px;
       .tips {
         color: #999;
       }
     }
     .tipsColor {
       color: #89888a;
+    }
+    .addMechanism {
+      width: 140px;
+      height: 30px;
+      background-color: #f0f0f0;
+      border-radius: 4px;
+      border: solid 1px #2e8ed6;
+      margin-bottom: 40px;
+      font-size: 12px;
+      line-height: 30px;
+      color: #2e8ed6;
+      text-align: center;
+      cursor: pointer;
     }
     .button {
       width: 300px;
@@ -108,13 +140,10 @@ export default {
       justify-content: center;
       padding-top: 20px;
       font-size: 12px;
+      color: #333;
       a {
         display: inline;
-        padding: 0 10px;
-        color: #333;
-      }
-      .registerLink {
-        color: #e47727;
+        color: #2e8ed6;
       }
     }
   }
