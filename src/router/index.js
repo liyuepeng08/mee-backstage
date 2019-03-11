3473173ce0f6cbf2fecbb840165be29e38739cfa
yuanna0323 <15010611339@163.com>
2019-03-11 19:12:44 +0800

    管理员邀请

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,8 @@ import AddStudent from '@/components/AddStudent/AddStudent' //添加学生
 import DetailStudent from '@/components/detailStudent/detailStudent' //管理员详情
 import AddTeacher from '@/components/AddTeacher/AddTeacher' //添加教师
 import DetailTeacher from '@/components/detailTeacher/detailTeacher' //教师详情
+import ReqManager from '@/components/reqManager/reqManager' //邀请管理员
+
 Vue.use(Router)
 
 export default new Router({
@@ -75,6 +77,10 @@ export default new Router({
         path: 'detailTeacher', //教师详情
         name: 'detailTeacher',
         component: DetailTeacher
+      }, {
+        path: 'reqManager', //教师详情
+        name: 'reqManager',
+        component: ReqManager
       }
     ]
   }, {
