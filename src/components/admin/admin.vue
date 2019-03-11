af75c5c23e99163dfec6ad310d26f67db0d82ba7
liyuepeng08 <wamk08@sina.com>
2019-03-11 14:24:08 +0800

    主体路由内容

--- src/components/admin/admin.vue
+++ src/components/admin/admin.vue
@@ -1,8 +1,10 @@
 <template>
     <section class="container">
-        <header></header>
-        <el-row>
-            <el-col :span="3">
+        <header>
+            <main-header></main-header>
+        </header>
+        <el-row class="cont">
+            <el-col :span="3" class="left-nav">
                 <left-nav></left-nav>
             </el-col>
             <el-col :span="21" style="background-color: #F3F3F5;">
@@ -14,14 +16,22 @@
 <script>
 //当前是 后台主页组件
 import LeftNav from '@/components/nav/nav'
+import mainHeader from '@/components/base/Header/Header'
 export default {
     components: {
-        LeftNav
+        LeftNav,
+        mainHeader
     }
 }
 </script>
 <style lang="less" scoped>
-
+.left-nav {
+    background-color: #3F4464;
+    height: 100%;
+}
+.container, .cont {
+    height: 100%;
+}
 </style>
 
 
