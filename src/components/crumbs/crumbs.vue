b7b3455d091da949b87e7a001e321786ce7a1807
zhucongcong <15110253002@163.com>
2019-03-11 15:15:41 +0800

    新增pages

--- src/components/crumbs/crumbs.vue
+++ src/components/crumbs/crumbs.vue
@@ -1,6 +1,6 @@
 <template>
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-        <i class="el-icon-location-outline"></i>
+    <el-breadcrumb class="position pos1" separator-class="el-icon-arrow-right">
+        <i class="posIcon el-icon-location-outline"></i>
         <el-breadcrumb-item :to="{ path: '/' }">校园管理</el-breadcrumb-item>
         <el-breadcrumb-item>教师管理</el-breadcrumb-item>
         <el-breadcrumb-item>新增教师</el-breadcrumb-item>
@@ -13,6 +13,14 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.position{
+    margin:20px 0 20px 20px;
+    padding-left: 14px;
+    .posIcon{
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+}
 </style>
 
