876859a952d21779e63c79065aa35761f6b3969a
zhucongcong <15110253002@163.com>
2019-03-20 18:23:20 +0800

    审核管理修改

--- src/components/studentManage/studentManage.vue
+++ src/components/studentManage/studentManage.vue
@@ -1,91 +1,76 @@
 <template>
-    <div class="teacherManager">
+    <div class="studentManager">
         <p class="topNav">
             <span>用户管理</span>
             <i class="el-icon-arrow-right"></i>
-            <span class="active">教师管理</span>
+            <span class="active">学生管理</span>
         </p>
-        <h2>教师管理</h2>
+        <h2>学生管理</h2>
         <div class="butt">
+            <el-button class="addBtn" type="primary" icon="el-icon-plus" @click="addStudent">新增</el-button>
+            <el-button class="addBtn" type="primary" icon="el-icon-share" @click="importBtn">导入</el-button>
+            <a href="#">
+                <i class="el-icon-download"></i>下载模板
+            </a>
             <el-button
-                class="teachBtn active"
+                class="addBtn teachBtn"
                 type="primary"
-                icon="el-icon-plus"
-                @click="addTeacher"
-            >新建</el-button>
-            <el-button class="teachBtn" type="primary" icon="el-icon-share" @click="invite">邀请</el-button>
+                icon="el-icon-share"
+                @click="invite"
+            >邀请</el-button>
         </div>
-        <el-table class="tablelInfo" :data="tableData">
+        <el-table class="studentInfo" :data="tableData">
             <el-table-column prop="name" label="名称"></el-table-column>
             <el-table-column prop="telephone" label="电话"></el-table-column>
-            <el-table-column prop="classes" label="所在班级"></el-table-column>
-            <el-table-column prop="subject" label="教授科目"></el-table-column>
+            <el-table-column prop="class" label="所在班级"></el-table-column>
             <el-table-column prop="mechanism" label="所属机构"></el-table-column>
             <el-table-column prop="time" label="创建时间"></el-table-column>
             <el-table-column label="操作">
                 <template slot-scope="scope">
-                    <el-button type="text" size="small" @click="detailMsg">详情</el-button>
+                    <el-button type="text" size="small" @click="detailMsg(scope)">详情</el-button>
                     <el-button type="text" size="small">编辑</el-button>
                     <el-button type="text" size="small" @click="deleteMsg(scope)">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
-        <pages/>
-        <!-- tip -->
-        <div class="tip" v-if="showTip">
-            <div class="info">
-                <img src="./tip.png" alt>
-                <span>操作成功，已通过审核！</span>
-            </div>
-        </div>
     </div>
 </template>
 <script>
-import Pages from '../pages/pages'
 export default {
-    mounted() {
-        if (this.$route.params.bool) {
-            this.showTip = true;
-            setTimeout(() => {
-                this.showTip = false;
-            }, 2000)
-        }
-    },
     data() {
         return {
-            showTip: false,
             tableData: [{
+                date: '2016-05-02',
                 name: '爱谁谁',
-                telephone: '18897689866',
-                classes: '猜猜看',
-                subject: '高数',
+                nickname: '我是代理商',
                 mechanism: '开普勒实验小学北京海淀分校',
+                telephone: '18897689866',
                 time: '2019-02-19',
-
+                class: '二班'
             }, {
+                date: '2016-05-02',
                 name: '爱谁谁',
-                telephone: '18897689866',
-                classes: '猜猜看',
-                subject: '高数',
+                nickname: '我是代理商',
                 mechanism: '开普勒实验小学北京海淀分校',
+                telephone: '18897689866',
                 time: '2019-02-19',
-
+                class: '二班'
             }, {
+                date: '2016-05-02',
                 name: '爱谁谁',
-                telephone: '18897689866',
-                classes: '猜猜看',
-                subject: '高数',
+                nickname: '我是代理商',
                 mechanism: '开普勒实验小学北京海淀分校',
+                telephone: '18897689866',
                 time: '2019-02-19',
-
+                class: '二班'
             }, {
+                date: '2016-05-02',
                 name: '爱谁谁',
-                telephone: '18897689866',
-                classes: '猜猜看',
-                subject: '高数',
+                nickname: '我是代理商',
                 mechanism: '开普勒实验小学北京海淀分校',
+                telephone: '18897689866',
                 time: '2019-02-19',
-
+                class: '二班'
             }]
         }
     },
@@ -104,32 +89,27 @@ export default {
         },
         detailMsg(scope) {
             this.$router.push({
-                path: '/admin/detailTeacher'
+                path: '/admin/detailStudent'
             })
         },
-        addTeacher() {
-            this.$router.push({
-                path: '/admin/addTeacher'
-            })
+        addStudent() {
+            this.$router.push({ path: 'addStudent' })
         },
-        invite() {//邀请
-            this.$router.push({
-                path: '/admin/reqManager'
-            })
+        importBtn() {
+            alert("你好啊")
+        },
+        invite() {
+            this.$router.push({ path: 'reqManager' })
         }
-    },
-    components: {
-        Pages
     }
 };
 </script>
 <style lang="less">
-.teacherManager {
+.studentManager {
     padding: 0 10px;
     .el-table th > .cell {
         font-weight: 700;
         color: #080808;
-        padding-left: 30px;
     }
     .topNav {
         font-size: 12px;
@@ -145,61 +125,39 @@ export default {
     }
     .butt {
         margin-top: 31px;
-        .teachBtn {
+        .addBtn {
             width: 90px;
             height: 30px;
-            color: #5693ff;
-            background: #f8fafc;
+            background: #5693ff;
             border-radius: 15px;
             border: solid 1px #5693ff;
             padding: 0;
+            box-shadow: 0px 4px 6px 0px rgba(86, 147, 255, 0.4);
+        }
+        .teachBtn {
+            background: #f8fafc;
+            color: #5693ff;
+            margin-left: 20px;
         }
         .active {
             color: #fff;
             background-color: #5693ff;
             box-shadow: 0px 4px 6px 0px rgba(86, 147, 255, 0.4);
         }
+        a {
+            font-size: 12px;
+            color: #5693ff;
+        }
     }
-    .tablelInfo {
+    .studentInfo {
         margin-top: 20px;
         border: 1px solid #f1f1f1;
-        border-bottom: none;
-        /deep/ tr {
-            background: #fff;
+        /deep/ th {
+            padding-left: 20px;
         }
         /deep/ .el-table__row {
             td {
                 padding-left: 20px;
-            }
-        }
-    }
-    .tip {
-        width: 360px;
-        height: 120px;
-        line-height: 120px;
-        background-color: #f4f8ff;
-        box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.1);
-        border-radius: 4px;
-        border: solid 1px #cacaca;
-        position: fixed;
-        top: 40%;
-        left: 40%;
-        z-index: 1;
-        .info {
-            width: 250px;
-            margin: 0 auto;
-            text-align: center;
-            img {
-                width: 30px;
-                height: 30px;
-                border-radius: 50%;
-                position: relative;
-                top: 8px;
-                right: 10px;
-            }
-            span {
-                font-size: 18px;
-                color: #080808;
             }
         }
     }
